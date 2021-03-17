88f1e24a157ebf0943ac37a1d3478996326ad602
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-03-17 14:19:36 +0100

    feat: add branching on PROTOC_TS_GENERATOR env var

--- main.go
+++ main.go
@@ -1,10 +1,26 @@
 package main
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/join-com/protoc-gen-ts/legacy/generator"
 )
 
 func main() {
-	g := generator.New()
-	g.Generate()
+	generatorVersion, isGeneratorConfigured := os.LookupEnv("PROTOC_TS_GENERATOR")
+	if !isGeneratorConfigured {
+		generatorVersion = "LEGACY"
+	}
+
+	if generatorVersion == "LEGACY" {
+		g := generator.New()
+		g.Generate()
+	} else if generatorVersion == "V2" {
+		// TODO
+	} else {
+		fmt.Fprintf(os.Stderr, "Invalid protoc-gen-ts generator version (%s)\n", generatorVersion)
+		os.Stderr.WriteString("Acceptable values for the 'PROTOC_TS_GENERATOR' environment variable are:\n\t- LEGACY\n\t- V2\n\n")
+		os.Exit(1)
+	}
 }

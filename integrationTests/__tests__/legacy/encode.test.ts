60d0a07cbf08c022418344126e4402cd1e900f7a
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-03-17 12:05:51 +0100

    test: move old tests to legacy directory

--- integrationTests/__tests__/legacy/encode.test.ts
+++ integrationTests/__tests__/legacy/encode.test.ts
@@ -1,14 +1,14 @@
+import * as path from 'path'
+import { Type, loadSync } from 'protobufjs'
+import { Foo } from './generated/Test'
+
 // TODO:
 // 1. deprecated log
 // 2. test changes in messages adding fields, removing
 // 3. test enums changes
 // 4. test if not sent
 
-import { Foo } from './generated/Test'
-import { loadSync } from 'protobufjs'
-import * as path from 'path'
-
-let PbTest: any
+let PbTest: Type
 beforeAll(() => {
   const root = loadSync(path.join(__dirname, 'proto', 'test.proto'))
   PbTest = root.lookupType('foo.Test')
@@ -16,7 +16,8 @@ beforeAll(() => {
 
 describe('encode', () => {
   let buffer: Uint8Array
-  let decoded: any
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  let decoded: Record<string, any>
   const values: Foo.ITest = {
     fieldInt32: 123,
     fieldInt32Repeated: [123, 12312],
@@ -47,21 +48,21 @@ describe('encode', () => {
     fieldEnum: 'UNKNOWN',
     fieldEnumRepeated: ['EDIT', 'VIEW'],
     message: {
-      title: 'msg'
+      title: 'msg',
     },
     messageRepeated: [
       {
-        title: 'msg1'
+        title: 'msg1',
       },
       {
-        title: 'msg2'
-      }
+        title: 'msg2',
+      },
     ],
     timestamp: new Date('2014-11-01T12:24:33.123'),
     timestampRepeated: [
       new Date('2014-02-26T12:00:00.234'),
-      new Date('2013-05-26T12:00:10.221')
-    ]
+      new Date('2013-05-26T12:00:10.221'),
+    ],
   }
 
   beforeEach(() => {
@@ -70,7 +71,7 @@ describe('encode', () => {
     decoded = PbTest.toObject(PbTest.decode(buffer), {
       enums: String,
       longs: Number,
-      defaults: false
+      defaults: false,
     })
   })
 
@@ -88,17 +89,19 @@ describe('encode', () => {
     'fieldString',
     'fieldEnum',
     // 'fieldBytes' // -- failed
-    'message'
-  ])('%s', fieldName => {
+    'message',
+  ])('%s', (fieldName: keyof typeof decoded) => {
     it(`encodes ${fieldName}`, () => {
       expect(decoded[fieldName]).toBeDefined()
-      expect(decoded[fieldName]).toEqual((values as any)[fieldName])
+      expect(decoded[fieldName]).toEqual(
+        values[fieldName as keyof typeof values]
+      )
     })
 
     it(`encodes ${fieldName}Repeated`, () => {
       const name = `${fieldName}Repeated`
       expect(decoded[name]).toBeDefined()
-      expect(decoded[name]).toEqual((values as any)[name])
+      expect(decoded[name]).toEqual(values[name as keyof typeof values])
     })
   })
 
@@ -109,29 +112,34 @@ describe('encode', () => {
       )
 
     it('encodes timestamp', () => {
-      expect(decoded.timestamp).toBeDefined()
-      expect(toDate(decoded.timestamp)).toEqual(values.timestamp)
+      expect(decoded['timestamp']).toBeDefined()
+      expect(toDate(decoded['timestamp'])).toEqual(values.timestamp)
     })
 
     it('encodes timestampRepeated', () => {
-      expect(decoded.timestampRepeated).toBeDefined()
-      expect(decoded.timestampRepeated.map(toDate)).toEqual(
-        values.timestampRepeated
-      )
+      expect(decoded['timestampRepeated']).toBeDefined()
+      expect(
+        (decoded['timestampRepeated'] as {
+          nanos: number
+          seconds: number
+        }[]).map(toDate)
+      ).toEqual(values.timestampRepeated)
     })
   })
 
   describe('fieldFloat', () => {
     it('encodes fieldFloat', () => {
-      expect(decoded.fieldFloat).toBeDefined()
-      expect(parseFloat(decoded.fieldFloat)).toEqual(
+      expect(decoded['fieldFloat']).toBeDefined()
+      expect(decoded['fieldFloat']).toEqual(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         Math.fround(values.fieldFloat!)
       )
     })
 
     it('encodes fieldFloatRepeated', () => {
-      expect(decoded.fieldFloatRepeated).toBeDefined()
-      expect(decoded.fieldFloatRepeated).toEqual(
+      expect(decoded['fieldFloatRepeated']).toBeDefined()
+      expect(decoded['fieldFloatRepeated']).toEqual(
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         values.fieldFloatRepeated!.map(Math.fround)
       )
     })

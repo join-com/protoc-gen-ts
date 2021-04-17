550e339ba071a765716cab6d5ee8f81be7781358
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-04-17 15:56:13 +0200

    build: run strip only locally in build.sh

--- build.sh
+++ build.sh
@@ -3,5 +3,7 @@
 # Building binary
 go build -o ./dist/protoc-gen-tsx
 
-# Stripping symbols to make the binary more lightweight
-strip -u -r -S ./dist/protoc-gen-tsx
+if [ "${CI}" != "true" ]; then
+    # Stripping symbols to make the binary more lightweight
+    strip -u -r -S ./dist/protoc-gen-tsx
+fi

a75521e454202be10264443c8f42bf15ac70c080
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-06-25 12:31:00 +0200

    feat: add support for other platforms

--- build.sh
+++ build.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
 # Building binary
-go build -o ./dist/protoc-gen-tsx
+env GOOS=darwin GOARCH=amd64 go build -o ./dist/protoc-gen-tsx.darwin.amd64
+env GOOS=darwin GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.darwin.arm64
+env GOOS=linux GOARCH=amd64 go build -o ./dist/protoc-gen-tsx.linux.amd64
+env GOOS=linux GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.linux.arm64
 
 if [ "${CI}" != "true" ]; then
     # Stripping symbols to make the binary more lightweight
-    strip -u -r -S ./dist/protoc-gen-tsx
+    strip -u -r -S ./dist/protoc-gen-tsx.darwin.amd64
 fi

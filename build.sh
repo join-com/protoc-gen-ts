951a857dc36f87a4d0d9bbcfa14d115e6a55ecf7
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-06-25 12:50:04 +0200

    build: select binary according to os & arch

--- build.sh
+++ build.sh
@@ -7,7 +7,19 @@ env GOOS=linux GOARCH=amd64 go build -o ./dist/protoc-gen-tsx.linux.amd64
 env GOOS=linux GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.linux.arm64
 
 if [ ! -f ./dist/protoc-gen-tsx ] && [ ! -L ./dist/protoc-gen-tsx ]; then
-  ln -s ./dist/protoc-gen-tsx.linux.arm64 ./dist/protoc-gen-tsx
+  if test "$(uname)" = "Darwin" ; then
+    if test "$(uname -m)" = "x86_64"; then
+        ln -s ./dist/protoc-gen-tsx.darwin.amd64 ./dist/protoc-gen-tsx
+    else
+        ln -s ./dist/protoc-gen-tsx.darwin.arm64 ./dist/protoc-gen-tsx ## Best effort guess
+    fi
+  elif test "$(uname)" = "Linux" ; then
+    if test "$(uname -m)" = "x86_64"; then
+        ln -s ./dist/protoc-gen-tsx.linux.amd64 ./dist/protoc-gen-tsx
+    else
+        ln -s ./dist/protoc-gen-tsx.linux.arm64 ./dist/protoc-gen-tsx ## Best effort guess
+    fi
+  fi
 fi
 
 chmod a+x ./dist/*

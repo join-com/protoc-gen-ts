f860cb33c2848a8f85df5b2af99a9281e6532f35
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-06-25 12:41:33 +0200

    fix: improve build.sh script

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ env GOOS=darwin GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.darwin.arm64
 env GOOS=linux GOARCH=amd64 go build -o ./dist/protoc-gen-tsx.linux.amd64
 env GOOS=linux GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.linux.arm64
 
-ln -s ./dist/protoc-gen-tsx.linux.arm64 ./dist/protoc-gen-tsx
+if [ ! -f ./dist/protoc-gen-tsx ] && [ ! -L ./dist/protoc-gen-tsx ]; then
+  ln -s ./dist/protoc-gen-tsx.linux.arm64 ./dist/protoc-gen-tsx
+fi
+
+chmod a+x ./dist/*
 
 if [ "${CI}" != "true" ]; then
     # Stripping symbols to make the binary more lightweight

08ade27b671ba91884c2de96aa6cca53c41de1fb
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-06-25 12:34:10 +0200

    build: fix test

--- build.sh
+++ build.sh
@@ -6,6 +6,8 @@ env GOOS=darwin GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.darwin.arm64
 env GOOS=linux GOARCH=amd64 go build -o ./dist/protoc-gen-tsx.linux.amd64
 env GOOS=linux GOARCH=arm64 go build -o ./dist/protoc-gen-tsx.linux.arm64
 
+ln -s ./dist/protoc-gen-tsx.linux.arm64 ./dist/protoc-gen-tsx
+
 if [ "${CI}" != "true" ]; then
     # Stripping symbols to make the binary more lightweight
     strip -u -r -S ./dist/protoc-gen-tsx.darwin.amd64

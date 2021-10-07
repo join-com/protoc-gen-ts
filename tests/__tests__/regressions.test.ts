8f3616f2389c74016ed2bc24e12db7b85c871335
Andres Correa Casablanca <andreu.casablanca@join.com>
2021-10-07 17:30:18 +0200

    fix: ensure that @deprecated is added to deprecated fields

--- tests/__tests__/regressions.test.ts
+++ tests/__tests__/regressions.test.ts
@@ -1,4 +1,11 @@
+// eslint-disable-next-line node/no-unpublished-import
+import * as ts from 'typescript'
+// eslint-disable-next-line node/no-unpublished-import
+import { InterfaceDeclaration, Project } from 'ts-morph'
 import { Regressions } from './generated/Regressions'
+// eslint-disable-next-line node/no-unpublished-import
+import { parse as parseComment } from 'comment-parser'
+import { join as pathJoin } from 'path'
 
 describe('regressions', () => {
   it('01. decodes missing nested fields as undefined when there are no enums', () => {
@@ -16,4 +23,84 @@ describe('regressions', () => {
     expect(reconstructedA.inner?.value).toBeUndefined()
     expect(reconstructedB.inner?.value).toBeUndefined()
   })
+
+  it('02. adds @deprecated jsdoc annotation to fields marked as deprecated in proto files', () => {
+    // Background: before this test was written, these annotations were only added when the message
+    //             itself was deprecated as well.
+
+    const tsConfigPath = pathJoin(__dirname, '..', 'tsconfig.json')
+    const project = new Project({ tsConfigFilePath: tsConfigPath })
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const regressionsFile = project.getSourceFile(
+      pathJoin(__dirname, 'generated', 'Regressions.ts')
+    )!
+    expect(regressionsFile).toBeDefined()
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const moduleDeclaration = regressionsFile.getFirstChildByKind(
+      ts.SyntaxKind.ModuleDeclaration
+    )!
+    expect(moduleDeclaration).toBeDefined()
+    expect(moduleDeclaration.getName()).toBe('Regressions')
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    const withDeprecatedFieldInterface = moduleDeclaration.getInterface(
+      'IWithDeprecatedField'
+    )!
+    expect(withDeprecatedFieldInterface).toBeDefined()
+    verifyThatDeprecatedFieldHasJSDocDeprecationAnnotation(
+      withDeprecatedFieldInterface
+    )
+    verifyThatNotDeprecatedFieldDoesNotHaveJSDocAnnotation(
+      withDeprecatedFieldInterface
+    )
+
+    const deprecatedWithDeprecatedFieldInterface =
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+      moduleDeclaration.getInterface('IDeprecatedWithDeprecatedField')!
+    expect(deprecatedWithDeprecatedFieldInterface).toBeDefined()
+    verifyThatDeprecatedFieldHasJSDocDeprecationAnnotation(
+      deprecatedWithDeprecatedFieldInterface
+    )
+    verifyThatNotDeprecatedFieldDoesNotHaveJSDocAnnotation(
+      deprecatedWithDeprecatedFieldInterface
+    )
+  })
 })
+
+function verifyThatDeprecatedFieldHasJSDocDeprecationAnnotation(
+  generatedInterface: InterfaceDeclaration
+): void {
+  const deprecatedField =
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    generatedInterface.getProperty('deprecated')!
+  expect(deprecatedField).toBeDefined()
+
+  const commentRanges = deprecatedField.getLeadingCommentRanges()
+  expect(commentRanges).toHaveLength(1)
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const parsedComment = parseComment(commentRanges[0]!.getText())
+  expect(parsedComment).toHaveLength(1)
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const commentBlock = parsedComment[0]!
+  expect(commentBlock.tags).toHaveLength(1)
+
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const tagSpec = commentBlock.tags[0]!
+  expect(tagSpec.tag).toBe('deprecated')
+}
+
+function verifyThatNotDeprecatedFieldDoesNotHaveJSDocAnnotation(
+  generatedInterface: InterfaceDeclaration
+): void {
+  const notDeprecatedField =
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    generatedInterface.getProperty('notDeprecated')!
+  expect(notDeprecatedField).toBeDefined()
+
+  const commentRanges = notDeprecatedField.getLeadingCommentRanges()
+  expect(commentRanges).toHaveLength(0)
+}
